9c7f8cea5b3665e97d8412198b91e3326def05d4
ClaudiusMinimus <claudiusminimus@gmail.com>
2022-10-27 21:08:42 -0600

    Update script.js
    added starter items

--- minecraft/kubejs/server_scripts/script.js
+++ minecraft/kubejs/server_scripts/script.js
@@ -22,10 +22,16 @@ onEvent('recipes', event => {
 
 })
 
-onEvent('item.tags', event => {
-	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
-	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
-
-	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
-	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
-})
+// Listen to player login event
+onEvent('player.logged_in', event => {
+	// Check if player doesn't have "starting_items" stage yet
+	if (!event.player.stages.has('starting_items')) {
+	  // Add the stage
+	  event.player.stages.add('starting_items')
+	  // Give some items to player
+	  event.player.give('minecraft:stone_sword')
+	  event.player.give('minecraft:stone_pickaxe')
+	  event.player.give('minecraft:stone_axe')
+	  event.player.give('30x minecraft:apple')
+	}
+  }) 
